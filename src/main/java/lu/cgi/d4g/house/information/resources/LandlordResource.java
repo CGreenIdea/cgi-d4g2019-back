28e9322722d8461fe256b1be6c3b56c965c3add7
Cyrille Chopelet <cyrille.chopelet@cgi.com>
2019-11-21 18:48:34 +0100

    Fix missing imports

--- src/main/java/lu/cgi/d4g/house/information/resources/LandlordResource.java
+++ src/main/java/lu/cgi/d4g/house/information/resources/LandlordResource.java
@@ -6,6 +6,7 @@ import lu.cgi.d4g.house.information.services.LandlordService;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
