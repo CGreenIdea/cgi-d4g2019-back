b2854ce130d281c8f070221e2f44c101b4790c53
aurore-simon <simon_aurore@hotmail.com>
2019-11-22 01:00:05 +0100

    Add DocumentEntity

---
+++ src/main/java/lu/cgi/d4g/DocumentEntity.java
@@ -0,0 +1,35 @@
+package lu.cgi.d4g;
+
+import lombok.Getter;
+import lombok.Setter;
+import lu.cgi.d4g.house.information.entities.HomeEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "document")
+public class DocumentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "home_id")
+    private HomeEntity home;
+
+    private String filename;
+
+    @Column(name = "localName", length = 36)
+    private String localName;
+
+}
