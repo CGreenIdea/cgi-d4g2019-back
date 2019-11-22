495be0b15d8dfb13fc7f5043061d4d7a0353662a
Cyrille Chopelet <cyrille.chopelet@cgi.com>
2019-11-22 04:12:53 +0100

    Restore getters

--- src/main/java/lu/cgi/d4g/document/dto/DocumentBean.java
+++ src/main/java/lu/cgi/d4g/document/dto/DocumentBean.java
@@ -1,10 +1,12 @@
 package lu.cgi.d4g.document.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.Instant;
 
 @Builder
+@Getter
 public class DocumentBean {
     private final long id;
     private final String title;
